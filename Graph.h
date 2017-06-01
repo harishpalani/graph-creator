b4d24b23dd0930268022c26aedb80252560f1078
Harish Palani <harishpalani3@gmail.com>
2017-06-01 01:48:36 +0000

    Initial commit

---
+++ Graph.h
@@ -0,0 +1,17 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+
+using namespace std;
+
+class Graph {
+  public:
+    Graph();
+    ~Graph();
+    
+  private:
+  
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,27 @@
+/**
+ * GRAPH CREATOR
+ * Graph creator with breadth first search.
+ * 
+ * Implementation: Create a program which can create graphs (we are using weighted edge, directed graphs), 
+ * and it can search for a path in the graph using breadth-first search. Use an adjacency table for the graph, 
+ * which can be printed out. (You may assume that no more than 20 vertices will be in the table.)
+ * 
+ * (1) Add Vertex: Enter a label for a vertex. Add it to the graph.
+ * (2) Add Edge: Enter two node names and a weight (number). Add an edge between them, 
+ *      from the first node to the second node. (You would need to create another edge to go both directions.)
+ * (3) Remove Vertex: Enter a vertex label and remove it from the graph. Remove all edges to that vertex.
+ * (4) Remove Edge: Enter two vertex labels, and remove an edge between them.
+ * (5) Find Path: Enter two vertex labels. Starting at the first label, use a breadth-first search to find a path between 
+ * the first vertex and the last vertex. Return the shortest path (and total) if it exists, or return that no path exists.
+ * 
+ * @author Harish Palani
+ * @version May 2017
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
