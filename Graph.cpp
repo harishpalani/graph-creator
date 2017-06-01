4f07238d38132cf9feb263ce415bd9b7d2f8bb15
Harish Palani <harishpalani3@gmail.com>
2017-06-01 20:34:07 +0000

    Setup complete & index extraction function implemented

--- Graph.cpp
+++ Graph.cpp
@@ -1,3 +1,28 @@
 #include "Graph.h"
  
 using namespace std;
+
+bool Graph::addVertex(const char* c) {
+    
+}
+
+bool Graph::removeVertex(const char* c) {
+    
+}
+
+bool Graph::addEdge(const char* c1, const char* c2) {
+    
+}
+
+bool Graph::removeEdge(const char* c1, const char* c2) {
+    
+}
+
+// Helpers
+int Graph::index(vector<char*> &content, const char* c) { // Gets index of char c within the graph
+    for (int i = 0; i < content.size(); i++) {
+        if (strcmp(content[i], c) == 0) {
+            return i;
+        }
+    }
+}
--- Graph.h
+++ Graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
@@ -9,9 +10,16 @@ class Graph {
   public:
     Graph();
     ~Graph();
+    bool addVertex(const char* c);
+    bool removeVertex(const char* c);
+    bool addEdge(const char* c1, const char* c2);
+    bool removeEdge(const char* c1, const char* c2);
     
   private:
-  
+    // Helpers
+    int index(vector<char*> &content, const char* c); // Get index of the desired content within the graph
+    // Variables
+    vector<char*> content;
 };
 
 #endif
